aae346d996b6f3f96b25b90cda64d0beb9bb972e
Davidson-09 <davidsonakra@gmail.com>
2022-07-02 12:32:17 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,38 @@
+import speech_recognition as sr
+import json
+from types import SimpleNamespace
+import re
+
+r = sr.Recognizer()
+
+Microphones = sr.Microphone.list_microphone_names()
+my_mic = sr.Microphone()
+
+# with sr.AudioFile(filename) as source:
+#     # listen for the data (load audio to memory)
+#     audio_data = r.record(source)
+#     # recognize (convert from speech to text)
+#     text = r.recognize_google(audio_data)
+#     print(text)
+
+with my_mic as source:
+    print("Say now!!!!")
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source)
+
+# print(r.recognize_google(audio))
+# print(audio)
+
+textObject = r.recognize_google(audio, language = 'en-IN', show_all = True )
+extract = json.dumps(textObject)
+text = json.loads(extract,object_hook=lambda d: SimpleNamespace(**d)).alternative[0].transcript
+print(text)
+
+# now to text recognition
+pattern = "[a-zA-Z0-9]+ [0-9]+:[0-9]+"
+secondPattern = "[a-zA-Z0-9]+ [0-9]+:[0-9]+\(-)[0-9]"
+
+if (re.search(pattern, text)):
+    print('valid input')
+else:
+    print('invalid input or wrong pattern')
